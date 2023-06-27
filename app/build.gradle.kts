467dec0003148eabfdb89d658c8b473a14d9cce6
OwenScott-GDS <olarosa@google.com>
2023-06-27 09:53:39 -0700

    Address gradle feedback

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,13 +69,14 @@ android {
 dependencies {
 
     implementation(platform("androidx.compose:compose-bom:2023.06.00"))
-    implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material3:material3-window-size-class")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
+
+    implementation("androidx.activity:activity-compose:1.7.2")
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
