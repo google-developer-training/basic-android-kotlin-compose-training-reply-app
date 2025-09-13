b9c2eefa3e077cb4bfb6b81df2832c84dd856b67
Akshay <akshaylalsp@gmail.com>
2025-09-13 17:32:12 +0530

    Merge pull request #99 from google-developer-training/starter_e2e_new
    Starter update E2E

--- app/src/main/java/com/example/reply/ui/ReplyDetailsScreen.kt
+++ app/src/main/java/com/example/reply/ui/ReplyDetailsScreen.kt
@@ -16,6 +16,7 @@
 package com.example.reply.ui
 
 import android.widget.Toast
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -57,6 +58,9 @@ fun ReplyDetailsScreen(
     onBackPressed: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    BackHandler {
+        onBackPressed()
+    }
     Box(modifier = modifier) {
         LazyColumn(
             contentPadding = WindowInsets.safeDrawing.asPaddingValues(),
@@ -171,6 +175,7 @@ private fun DetailsScreenButtonBar(
     displayToast: (String) -> Unit,
     modifier: Modifier = Modifier
 ) {
+
     Box(modifier = modifier) {
         when (mailboxType) {
             MailboxType.Drafts ->
